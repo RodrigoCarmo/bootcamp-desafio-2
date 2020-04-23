09a8ea0f16f9b8d7728e8aa8ee2b197e791e9db2
RodrigoCarmo <roo.carmo@gmail.com>
2020-04-23 14:07:08 -0300

    Repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,97 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+    return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body;
+
+    const newProject = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0,
+    };
+
+    repositories.push(newProject);    
+
+    return response.json(newProject);    
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  const searchId = repositories.find(r => r.id === id ); 
+
+  if (searchId === undefined) {
+    return  response.status(400).json();
+    }
+
+  const likes = repositories[repositorieIndex].likes;    
+    
+  const alterRepositorie = {
+      id,
+      url,
+      title,
+      techs,
+      likes
+    }
+
+  repositories[repositorieIndex] = alterRepositorie;
+  return response.json(alterRepositorie);   
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  const searchId = repositories.find(r => r.id === id );
+  
+  if (searchId === undefined) {
+    return response.status(400).json();
+    
+      }
+  
+    
+  function remove(){
+    repositories.splice(repositorieIndex, 1);
+    return response.status(204).json();
+  }
+  
+  remove(); 
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+ const { id } = request.params;
+
+ const searchId = repositories.find(r => r.id === id );
+
+ if (searchId === undefined) {
+  
+   return response.status(400).json();
+   
+ };
+
+
+ const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+var like = repositories[repositorieIndex].likes ;
+
+like = like + 1;
+
+repositories[repositorieIndex].likes = like;
+
+ return response.json(repositories[repositorieIndex]);
+
+
 });
 
 module.exports = app;
